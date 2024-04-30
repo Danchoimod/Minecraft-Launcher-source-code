a6f7d7c83225ae96a35ba053ae22d3609a6f16be
Danchoimod <113335341+Danchoimod@users.noreply.github.com>
2024-04-30 14:24:34 +0700

    Update

---
+++ loadingScreenActivity.java
@@ -0,0 +1,173 @@
+package com.mojang.minecraftVN;
+
+import android.animation.*;
+import android.app.*;
+import android.app.Activity;
+import android.content.*;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.net.Uri;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.webkit.*;
+import com.blogspot.atifsoftwares.animatoolib.*;
+import com.bumptech.glide.Glide;
+import com.google.firebase.FirebaseApp;
+import com.zip4j.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.*;
+import org.json.*;
+
+public class LoadingscreenActivity extends AppCompatActivity {
+	
+	private Timer _timer = new Timer();
+	
+	private String url = "";
+	
+	private LinearLayout linear1;
+	private ImageView imageview2;
+	private ImageView loadingview;
+	
+	private Intent i = new Intent();
+	private TimerTask t;
+	private SharedPreferences file;
+	private SharedPreferences jv;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.loadingscreen);
+		initialize(_savedInstanceState);
+		FirebaseApp.initializeApp(this);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		linear1 = findViewById(R.id.linear1);
+		imageview2 = findViewById(R.id.imageview2);
+		loadingview = findViewById(R.id.loadingview);
+		file = getSharedPreferences("f", Activity.MODE_PRIVATE);
+		jv = getSharedPreferences("jv", Activity.MODE_PRIVATE);
+	}
+	
+	private void initializeLogic() {
+		Glide.with(getApplicationContext()).load(Uri.parse("file:///android_asset/".concat("grass.gif"))).into(loadingview);
+		/*
+getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+View.SYSTEM_UI_FLAG_HIDE_NAVIGATION| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+Window w = this.getWindow();
+w.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+w.setStatusBarColor(Color.parseColor("#33000000"));
+}
+*/
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){ getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS); }View decorView = getWindow().getDecorView();
+		// Hide both the navigation bar and the status bar.
+		// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+		// a general rule, you should design your app to hide the status bar whenever you
+		// hide the navigation bar.
+		int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+		decorView.setSystemUiVisibility(uiOptions);
+		t = new TimerTask() {
+			@Override
+			public void run() {
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						if (jv.contains("jv")) {
+							startActivity(new Intent(LoadingscreenActivity.this, JavalauncherActivity.class)); Animatoo.animateInAndOut(LoadingscreenActivity.this);
+						}
+						else {
+							i.setClass(getApplicationContext(), MainmenuActivity.class);
+							startActivity(i);
+							overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+						}
+						finish();
+					}
+				});
+			}
+		};
+		_timer.schedule(t, (int)(3000));
+	}
+	
+	@Override
+	public void onBackPressed() {
+		
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
