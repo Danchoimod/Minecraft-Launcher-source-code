ffdd3144cfb3d3e88998963273f1b9777190fa70
Danchoimod <113335341+Danchoimod@users.noreply.github.com>
2024-04-30 14:23:03 +0700

    Update

---
+++ splashScreenActivity.java
@@ -0,0 +1,164 @@
+package com.mojang.minecraftVN;
+
+import android.animation.*;
+import android.app.*;
+import android.content.*;
+import android.content.Intent;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.media.MediaPlayer;
+import android.net.*;
+import android.net.Uri;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.webkit.*;
+import com.blogspot.atifsoftwares.animatoolib.*;
+import com.bumptech.glide.Glide;
+import com.google.firebase.FirebaseApp;
+import com.zip4j.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.*;
+import org.json.*;
+
+public class SplashscreenActivity extends AppCompatActivity {
+	
+	private Timer _timer = new Timer();
+	
+	private double progress = 0;
+	
+	private LinearLayout linear1;
+	private LinearLayout linear2;
+	private ImageView mojang;
+	private ProgressBar progressbar1;
+	
+	private Intent i = new Intent();
+	private TimerTask t;
+	private MediaPlayer media;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.splashscreen);
+		initialize(_savedInstanceState);
+		FirebaseApp.initializeApp(this);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		linear1 = findViewById(R.id.linear1);
+		linear2 = findViewById(R.id.linear2);
+		mojang = findViewById(R.id.mojang);
+		progressbar1 = findViewById(R.id.progressbar1);
+	}
+	
+	private void initializeLogic() {
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+		            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+		            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+		            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+		            | View.SYSTEM_UI_FLAG_FULLSCREEN
+		            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+		progress = 0;
+		Glide.with(getApplicationContext()).load(Uri.parse("file:///android_asset/".concat("mojang.gif"))).into(mojang);
+		t = new TimerTask() {
+			@Override
+			public void run() {
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						progress++;
+						if (progress == 10) {
+							Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.mojang.minecraftpe"); startActivity(launchIntent);
+						}
+						else {
+							progressbar1.setProgress((int)progress);
+							progressbar1.getProgressDrawable().setColorFilter(
+							    Color.WHITE, android.graphics.PorterDuff.Mode.SRC_IN);
+							
+						}
+					}
+				});
+			}
+		};
+		_timer.scheduleAtFixedRate(t, (int)(0), (int)(150));
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){ getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS); }
+	}
+	
+	@Override
+	public void onBackPressed() {
+		t.cancel();
+		finish();
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
